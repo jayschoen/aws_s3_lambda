de3b2eb1b8db315bb16f8c2696c280ec2500ad88
Jay Schoen <jay.r.schoen@gmail.com>
2017-08-15 09:13:46 -0400

    add mechanisms to contact s3

--- tiny_server.py
+++ tiny_server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+import boto3
+
 app = Flask(__name__)
 
 @app.route('/test')
@@ -13,7 +15,23 @@ def index():
 
   # check if a submission happened
   if request.method == 'POST':
-    message = 'submit test'
+    
+    # connect to amazon s3
+    s3 = boto3.resource('s3')
+
+    # loop over submitted files list
+    data_files = request.files.getlist('file[]')
+    for data_file in data_files:
+
+      file_contents = data_file.read()
+
+      # upload file to bucket->folder
+      #s3.Bucket('nusa-dev-testing').put_object(Key='jschoen/' + data_file.filename, Body=file_contents)
+
+      #upload file to bucket
+      s3.Bucket('nusa-jschoen-resizeimage-origin').put_object(Key=data_file.filename, Body=file_contents)
+
+    message = 'submitted!'
 
   # render_template() looks in the templates directory
   return render_template('upload_page.html', message = message)
