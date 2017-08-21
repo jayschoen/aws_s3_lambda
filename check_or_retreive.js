0a60f071c49a7d67e3de55ed94b5fc87911dfb10
Jay Schoen <jay.r.schoen@gmail.com>
2017-08-21 11:08:02 -0400

    add some comments; move observable into callable function

--- check_or_retreive.js
+++ check_or_retreive.js
@@ -1,18 +1,23 @@
-myObservable = Rx.Observable
-	// time a timer... 0 == start immediately, 5000 == every 5 seconds do X
-	.timer(0, 5000)
-	.switchMap(response => fetch("http://127.0.0.1:5000/status"))
-	.flatMap(response => response.text())
-	.filter(response => response === 'exit') 
-	.take(1)
-	.timeout(15000);
+function status_loop() {
 
-myObservable.subscribe(
-	// arg1 = emitted, arg2 = error, arg3 = completed (only happens if no error!)
-	// emitted data
-	response => console.log(response),
-	// error
-	error => console.log(error),
-	// completed
-	() => console.log('completed')
-); 
+    myObservable = Rx.Observable
+        // time a timer... 0 == start immediately, 5000 == every 5 seconds do X
+        .timer(0, 5000)
+        // turn the response object into another observable...
+        .switchMap(response => fetch("http://127.0.0.1:5000/status"))
+        .flatMap(response => response.text())
+        .filter(response => response === 'exit') 
+        .take(1)
+        .timeout(15000);
+
+    myObservable.subscribe(
+        // arg1 = emitted, arg2 = error, arg3 = completed (only happens if no error!)
+        // emitted data
+        response => console.log(response),
+        // error
+        error => console.log(error),
+        // completed
+        () => console.log('completed')
+    ); 
+
+}
